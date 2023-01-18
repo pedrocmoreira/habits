af81c0814dabb56bee2a134aa9bffa2e0df5ad7c
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-18 14:56:37 -0300

    chore: criando componentes para habitDay e Day

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
-import './styles/global.css'
-import { Habit } from "./components/Habit";
+import {Header} from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
+import './styles/global.css';
 
-export default function App(){
-  return(
-    <div className="bg-zinc-900 w-10 h-10 text-white rouded m-2 flex items-center justify-center">
-      <Habit completed={3}/>
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header/>
+        <SummaryTable/>
+      </div>
     </div>
   )
 }
