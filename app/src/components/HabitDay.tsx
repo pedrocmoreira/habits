8a269bc93e987e1c669c3ac8cfd9b81cb6696b66
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-18 16:18:58 -0300

    chore: finalizando componente HabitDay

--- app/src/components/HabitDay.tsx
+++ app/src/components/HabitDay.tsx
@@ -7,11 +7,11 @@ export const day_margin_between = 8;
 export const day_size = (Dimensions.get('screen').width / week_days) - (screen_horizontal_padding + 5);
 
 export function HabitDay(){
-
   return(
     <TouchableOpacity
       className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800'
       style={{width: day_size, height: day_size}}
+      activeOpacity={.7}
     />
   )
 }
--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,21 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { HabitDay, day_size } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { generateDatesFromYearStart } from "../utils/generate-dates-from-year-start";
 
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 
-export function Home(){
-  return(
+export function Home() {
+  const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+  const datesFromYearStart = generateDatesFromYearStart();
+  const minimumSummaryDatesSizes = 18 * 7;
+  const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+
+  return (
     <View className='flex-1 bg-background px-8 pt-16'>
-      <Header/>
+      <Header />
 
       <View className="flex-row mt-6 mb-2">
         {
           weekDays.map((weekDay, i) => (
-            <Text 
+            <Text
               key={`${weekDay}-${i}`}
               className='text-zinc-400 text-xl font-bold text-center mx-1'
-              style={{width: day_size}}
+              style={{ width: day_size }}
             >
               {weekDay}
             </Text>
@@ -23,7 +29,33 @@ export function Home(){
         }
       </View>
 
-      <HabitDay/>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 25}}
+      >
+        <View className='flex-row flex-wrap'>
+          {
+            datesFromYearStart.map(date => (
+              <HabitDay
+                key={date.toISOString()}
+              />
+            ))
+          }
+
+          {
+            amountOfDaysToFill > 0 && Array
+              .from({ length: amountOfDaysToFill })
+              .map((_, i) => (
+                <View
+                  className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
+                  style={{ width: day_size, height: day_size }}
+                />
+              ))
+          }
+        </View>
+      </ScrollView>
+
+
     </View>
   )
 }
