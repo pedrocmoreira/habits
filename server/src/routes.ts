9d7556f9943857b1a22144fb36c697dcb77cd5e7
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-18 20:23:03 -0300

    feat: adicionando rota para marcar habito como concluído

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -71,5 +71,55 @@ export async function appRoutes(app: FastifyInstance) {
       possibleHabits,
       completedHabits,
     }
-  })
+  });
+
+  app.patch('/habits/:id/toggle',async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findFirst({
+      where: {
+        date: today,
+      }
+    });
+
+    if(!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      }
+    })
+
+    if(dayHabit) {
+      //remover a marcação de completo 
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      })
+    }else{
+      //cria a marcação de hábito completo
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      })
+    }
+  });
 }
