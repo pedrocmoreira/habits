8eccbe19cc17a367f2a17519456198a40341681d
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-18 15:22:35 -0300

    chore: finalizando contagem de dias na tabela

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -7,6 +7,10 @@ export function SummaryTable() {
 
   const summaryDates = generateDatesFromYearBeginning()
 
+  const minimumSummaryDatesSize = 18 * 7; //18 weeks
+
+  const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
   return (
     <div className='w-full flex'>
 
@@ -26,6 +30,14 @@ export function SummaryTable() {
         {summaryDates.map(date => {
           return <HabitDay key={date.toString()}/>
         })}
+
+        {
+          amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
+            return(
+              <div key={i} className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'/>
+            )
+          })
+        }
       </div>
     </div>
   )
