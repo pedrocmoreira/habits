c6c577ef86b5e009a04e6da33ab1de6c5cd0f3ff
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-18 20:58:34 -0300

    feat: adicionando rota para resumo de hábitos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -122,4 +122,38 @@ export async function appRoutes(app: FastifyInstance) {
       })
     }
   });
+
+  
+  app.get('/summary', async () => {
+    //retornar um resumo com uma lista de  várias informações
+
+    // completed = habitos completos
+    //amount = quantidade de hábitos no dia
+
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+           cast(count(*) as float) 
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT 
+           cast(count(*) as float) 
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE 
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+
+      FROM days D
+    `;
+
+    return summary
+  })
 }
