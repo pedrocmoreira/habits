f9470a2157658f863952a27fedbe464949fd7dde
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-18 21:51:48 -0300

    chore: criado modal para criação de hábito

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
 import { Plus, X } from 'phosphor-react';
 import * as Dialog from '@radix-ui/react-dialog';
 
+import { NewHabitForm } from './NewHabitForm';
+
 import logoImage from '../assets/logo.svg';
+
 
 export function Header() {
   return (
@@ -24,6 +26,12 @@ export function Header() {
             <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200'>
               <X size={24}  aria-label="Fechar"/>
             </Dialog.Close>
+
+            <Dialog.Title className='text-3xl leading-tight font-extrabold'>
+              Criar hábito
+            </Dialog.Title>
+
+            <NewHabitForm/>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
