fdfaf4dcfb8a5aebd4e7652b01642623fc8e3061
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-20 11:28:42 -0300

    chore: criando checkboxes

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,4 +1,6 @@
-import * as Popover from '@radix-ui/react-popover'
+import * as Popover from '@radix-ui/react-popover';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
 import clsx from 'clsx';
 
 import { ProgressBar } from './ProgressBar'
@@ -8,25 +10,41 @@ interface HabitDayProps {
   amount: number
 }
 
-export function HabitDay({completed, amount}:HabitDayProps) {
-  const completedPercentage = Math.round((completed/amount * 100));
+export function HabitDay({ completed, amount }: HabitDayProps) {
+  const completedPercentage = Math.round((completed / amount * 100));
 
   return (
     <Popover.Root>
       <Popover.Trigger className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg', {
-        'bg-zinc-900 border-zinc-800':completedPercentage === 0,
-        'bg-violet-900 border-violet-700':completedPercentage >= 0 && completedPercentage < 20,
-        'bg-violet-800 border-violet-600':completedPercentage >= 20 && completedPercentage < 40,
-        'bg-violet-700 border-violet-500':completedPercentage >= 40 && completedPercentage < 60,
-        'bg-violet-600 border-violet-500':completedPercentage >= 60 && completedPercentage < 80,
-        'bg-violet-500 border-violet-400':completedPercentage >= 80,
+        'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+        'bg-violet-900 border-violet-700': completedPercentage >= 0 && completedPercentage < 20,
+        'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+        'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+        'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+        'bg-violet-500 border-violet-400': completedPercentage >= 80,
       })} />
       <Popover.Portal>
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
           <span className='font-semibold text-zinc-400'>quarta-feira</span>
           <span className='mt-1 font-extrabold leading-tight text-2xl'>18/01</span>
 
-          <ProgressBar progress={completedPercentage}/>
+          <ProgressBar progress={completedPercentage} />
+
+          <div className='mt-6 flex flex-col gap-3'>
+            <Checkbox.Root
+              className='flex items-center gap-3 group'
+            >
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                 <Checkbox.Indicator>
+                    <Check size={20} className='text-white'/>  
+                </Checkbox.Indicator>
+              </div>
+              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-500'>
+                Beber 2L de água
+              </span>
+
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow width={8} height={8} className='fill-zinc-900' />
         </Popover.Content>
