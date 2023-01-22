755845e0b9e21f3f12a27b8434c5861f7bd88b2c
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-22 14:07:34 -0300

    chore: fix, array vazio quando não tem nenhum hábito dentro da rota

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,10 +1,10 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
-import { Check } from 'phosphor-react';
 import clsx from 'clsx';
 
 import { ProgressBar } from './ProgressBar'
 import dayjs from 'dayjs';
+import { useEffect } from 'react';
+import { HabitsList } from './HabitsList';
 
 interface HabitDayProps {
   date: Date;
@@ -35,21 +35,7 @@ export function HabitDay({ date, completed = 0, amount = 0 }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className='mt-6 flex flex-col gap-3'>
-            <Checkbox.Root
-              className='flex items-center gap-3 group'
-            >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-                <Checkbox.Indicator>
-                  <Check size={20} className='text-white' />
-                </Checkbox.Indicator>
-              </div>
-              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-500'>
-                Beber 2L de água
-              </span>
-
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} />
 
           <Popover.Arrow width={8} height={8} className='fill-zinc-900' />
         </Popover.Content>
