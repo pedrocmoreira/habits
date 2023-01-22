df13a30472c108310820330dae235100d8e95a7d
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-22 12:56:21 -0300

    feat: adicionando conexão com api para o cadastro de novos hábitos

--- app/src/screens/NewHabit.tsx
+++ app/src/screens/NewHabit.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
-import { View, ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
 
+import { api } from '../lib/axios';
+
 export function NewHabit() {
   const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  const [title, setTitle] = useState('');
 
   const avaiableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabádo']
 
@@ -16,6 +20,25 @@ export function NewHabit() {
       setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex]);
+    }
+  }
+
+  async function handleCreateNewHabit(){
+    try {
+      if(!title.trim() || weekDays.length === 0 ){
+        Alert.alert('Novo hábito', 'Por favor, adicione um título para o seu novo hábito e escolha a peridiocidade.');
+      }
+
+      await api.post('/habits', {title, weekDays});
+
+      setTitle('');
+      setWeekDays([]);
+
+
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito');
     }
   }
 
@@ -35,6 +58,8 @@ export function NewHabit() {
             className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
             placeholder='ex.: exercicios, dormir bem, etc...'
             placeholderTextColor={colors.zinc[400]}
+            onChangeText={setTitle}
+            value={title}
           />
 
           <Text className='font-semibold mt-4 mb-3 text-white text-base'>
@@ -55,6 +80,7 @@ export function NewHabit() {
           <TouchableOpacity
             activeOpacity={.7}
             className='w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6'
+            onPress={handleCreateNewHabit}
           >
             <Feather
               name='check'
