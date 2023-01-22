023e8fcc9ed2527378758ef0e3b9788bf735e7a0
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-22 15:01:01 -0300

    chore: adicionando transitions e acessibilidade nos botões

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 
 import { ProgressBar } from './ProgressBar'
 import dayjs from 'dayjs';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { HabitsList } from './HabitsList';
 
 interface HabitDayProps {
@@ -26,7 +26,7 @@ export function HabitDay({ date, defaultCompleted = 0, amount = 0 }: HabitDayPro
 
   return (
     <Popover.Root>
-      <Popover.Trigger className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg", {
+      <Popover.Trigger className={clsx("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background", {
         'bg-zinc-900 border-zinc-800': completedPercentage === 0,
         'bg-violet-900 border-violet-500': completedPercentage > 0 && completedPercentage < 20,
         'bg-violet-800 border-violet-500': completedPercentage >= 20 && completedPercentage < 40,
