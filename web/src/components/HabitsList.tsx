5e410ad0ad1db5778599cbecded7149e5d3f54bb
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-22 14:28:59 -0300

    feat: adicionando api para marcar/desmarcar hábito

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,4 +1,5 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
+import dayjs from 'dayjs';
 import { Check } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
@@ -19,6 +20,27 @@ interface HabitsInfo {
 export function HabitsList({ date }: HabitsListProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
 
+  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date);
+
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`/habits/${habitId}/toggle`);
+
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+
+    let completedHabits: string[] = [];
+
+    if(isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
+    }else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    })
+  }
+
   useEffect(() => {
     api.get('day', {
       params: {
@@ -33,7 +55,9 @@ export function HabitsList({ date }: HabitsListProps) {
         return (
           <Checkbox.Root
             key={habit.id}
+            onCheckedChange={() => {handleToggleHabit(habit.id)}}
             checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
             className='flex items-center gap-3 group'
           >
             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
